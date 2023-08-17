1e9b7dcc1f32c1cf4b24ab028247bfb1f3bedce8
muralivijay <muralidharan9845@gmail.com>
2023-08-17 04:47:37 +0000

    techpack:display:dsi: Import gamma support from veux.
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>
    Signed-off-by: fadlyas07 <mhmmdfdlyas@proton.me>

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -229,6 +229,7 @@ struct dsi_panel {
 	int power_mode;
 	enum dsi_panel_physical_type panel_type;
 	int hbm_mode;
+	u32 dsi_refresh_flag;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -348,6 +349,9 @@ void dsi_panel_ext_bridge_put(struct dsi_panel *panel);
 
 void dsi_panel_calc_dsi_transfer_time(struct dsi_host_common_cfg *config,
 		struct dsi_display_mode *mode, u32 frame_threshold_us);
+
+void dsi_set_backlight_control(struct dsi_panel *panel,
+			 struct dsi_display_mode *adj_mode);
 
 int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
 
