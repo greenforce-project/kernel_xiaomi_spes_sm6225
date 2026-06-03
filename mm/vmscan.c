1587dcf931953a2d1c45772c651b58275533fd9c
fadlyas07 <mhmmdfdlyas@gmail.com>
2026-06-03 07:05:29 +0000

    fixup! BACKPORT: mmap locking API: rename mmap_sem to mmap_lock
    Change-Id: Ie96e12e16b11a7621341a8df0ac84ba2e57ffa66

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3616,13 +3616,13 @@ static void walk_mm(struct lruvec *lruvec, struct mm_struct *mm, struct lru_gen_
 			break;
 
 		/* the caller might be holding the lock for write */
-		if (down_read_trylock(&mm->mmap_sem)) {
+		if (down_read_trylock(&mm->mmap_lock)) {
 			unsigned long start = walk->next_addr;
 			unsigned long end = mm->highest_vm_end;
 
 			err = walk_page_range(start, end, &args);
 
-			up_read(&mm->mmap_sem);
+			up_read(&mm->mmap_lock);
 
 			if (walk->batched) {
 				spin_lock_irq(&pgdat->lru_lock);
